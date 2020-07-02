91ba196fbcc38f530eb711d2d64baf4e54d97e0a
fdcoder81 <carloanselmi81@gmail.com>
2020-07-02 16:41:03 +0100

    Updates

---
+++ precache-manifest.b5e28202a7742a3002bbafc1a0008885.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "012357bc268b8f9da0efb77e906b3472",
+    "url": "/techshop/index.html"
+  },
+  {
+    "revision": "666a76dddd2fbd62cea1",
+    "url": "/techshop/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "1cf6662e367f6197642a",
+    "url": "/techshop/static/css/main.38fccaff.chunk.css"
+  },
+  {
+    "revision": "666a76dddd2fbd62cea1",
+    "url": "/techshop/static/js/2.bd0c3069.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/techshop/static/js/2.bd0c3069.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cf6662e367f6197642a",
+    "url": "/techshop/static/js/main.2e3cd1ef.chunk.js"
+  },
+  {
+    "revision": "0886ed52774124182bde",
+    "url": "/techshop/static/js/runtime-main.78778bd7.js"
+  },
+  {
+    "revision": "1da1758680c547af96f8e7b723b48232",
+    "url": "/techshop/static/media/1.1da17586.jpg"
+  },
+  {
+    "revision": "4b1ec88607bf2c8b6997e038c62672a1",
+    "url": "/techshop/static/media/4.4b1ec886.jpg"
+  },
+  {
+    "revision": "efe34abf6f59bbfee43e752e732b0541",
+    "url": "/techshop/static/media/5.efe34abf.jpg"
+  },
+  {
+    "revision": "3487cfba5abd36452488ff6bcee71f04",
+    "url": "/techshop/static/media/laptops-cat.3487cfba.jpg"
+  },
+  {
+    "revision": "fe848f12d8a7885da87677f03c723a3d",
+    "url": "/techshop/static/media/monitor1.fe848f12.jpg"
+  },
+  {
+    "revision": "efb00595c4ec1a466746218ef62eaab8",
+    "url": "/techshop/static/media/monitor2.efb00595.jpg"
+  },
+  {
+    "revision": "7b5fe85ba2efc23d06cb54025c7647b2",
+    "url": "/techshop/static/media/monitor3.7b5fe85b.jpg"
+  },
+  {
+    "revision": "35d0dc81593caa1aa568980d914ea68d",
+    "url": "/techshop/static/media/monitor4.35d0dc81.jpg"
+  },
+  {
+    "revision": "46470d3bc75adb7491c686266c95309e",
+    "url": "/techshop/static/media/monitor5.46470d3b.jpg"
+  },
+  {
+    "revision": "52640bdaae77c80e148f4baabf82792b",
+    "url": "/techshop/static/media/monitors-cat.52640bda.jpg"
+  },
+  {
+    "revision": "f9e938cc843faf8d2059d13ca1347ca6",
+    "url": "/techshop/static/media/pc1.f9e938cc.jpg"
+  },
+  {
+    "revision": "6b7dda38edb5e1a8094ab9cc9a7030dc",
+    "url": "/techshop/static/media/pc2.6b7dda38.jpg"
+  },
+  {
+    "revision": "92252d13417485687712d0b4803f1968",
+    "url": "/techshop/static/media/pc3.92252d13.jpg"
+  },
+  {
+    "revision": "02100f1d6a5837c4a701623a304df1ac",
+    "url": "/techshop/static/media/pc4.02100f1d.jpg"
+  },
+  {
+    "revision": "e759bcdb65a0dcc266df56eed39bb4a9",
+    "url": "/techshop/static/media/pc5.e759bcdb.jpg"
+  },
+  {
+    "revision": "afc8f18ae20bcadafb0b9ee1df0c2754",
+    "url": "/techshop/static/media/pcs-cat.afc8f18a.jpg"
+  },
+  {
+    "revision": "6497d9c82fae6e034d1e14a9fd8e065f",
+    "url": "/techshop/static/media/tablet1.6497d9c8.jpg"
+  },
+  {
+    "revision": "60fb8da84aeada3a3b7849a5058be6ac",
+    "url": "/techshop/static/media/tablet2.60fb8da8.jpg"
+  },
+  {
+    "revision": "bb4a34b5128ec0756ae9764a92ff2433",
+    "url": "/techshop/static/media/tablet3.bb4a34b5.jpg"
+  },
+  {
+    "revision": "9d26d2ff5e36b1f0d112babc66970889",
+    "url": "/techshop/static/media/tablet4.9d26d2ff.jpg"
+  },
+  {
+    "revision": "18f040e1cffa42850c3b6c8e94f661f6",
+    "url": "/techshop/static/media/tablet5.18f040e1.jpg"
+  },
+  {
+    "revision": "6d0c547c8c60fc2d50cfdb18c5c970a5",
+    "url": "/techshop/static/media/tablets-cat.6d0c547c.jpg"
+  }
+]);
